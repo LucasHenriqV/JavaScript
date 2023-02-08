6667b8088f337e69661c1557b98165d3f5f699ae
unknown <ukssbrs@gmail.com>
2023-02-07 23:30:11 -0300

    praticando meus conhecimentos

---
+++ praticando/index.js
@@ -0,0 +1,20 @@
+var n1 = 10
+
+var n2 = 20
+
+var n3 = null
+
+
+n3 = n1
+n1 = n2
+n2 = n3
+
+
+
+
+document.write('n1:' + n1 + '<br/>')
+document.write('n2:' + n2 + '<br/>')
+document.write('n3:' + n3 + '<br/>')
+
+
+console.log(n1, n2, n3)
