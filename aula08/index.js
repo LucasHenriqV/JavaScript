bcb228421c18f9487d34b81e1656f8779beb5e92
unknown <ukssbrs@gmail.com>
2023-02-09 22:29:10 -0300

    operadoresIfElse

---
+++ aula08/index.js
@@ -0,0 +1,6 @@
+/*if(10<9){
+document.write('verdadeiro')
+} 
+else{
+document.write('falso')
+}
---
+++ aula08/pasta/js2.js
@@ -0,0 +1,10 @@
+let nota = prompt('Digite sua nota')
+const media = 6
+
+if(nota >= media){
+document.write('aprovado')
+}
+else{
+document.write('reprovado')
+}
+console.log 
