5e62b874b62f46b01d065c5618d2057182cb2b2a
unknown <ukssbrs@gmail.com>
2023-02-12 09:33:01 -0300

    operadoresLogicosIfelse

---
+++ aula09/index.js
@@ -0,0 +1,8 @@
+/*let variavel1 = 10
+let variavel2 = 40
+
+//variavel1 = parseInt(variavel1)
+//variavel2 = parseInt(variavel2)
+
+document.write(variavel1.toString() + variavel2.toString()) */
+
---
+++ aula10/index.js
@@ -0,0 +1,59 @@
+
+/* operador "E" && verdadeiro de todas as expressões forem verdadeiras
+
+let nota = prompt("digite sua nota")
+const media = 6
+let presente = prompt("digite dias presentes")
+const presença = 330
+
+if(nota >= media && presente >= presença){
+document.write('APROVADO')
+}
+else{
+document.write('REPROVADO')
+}*/
+
+/* operador "OU"|| verdadeiro se pelo menos uma das expressões for verdadeira
+
+let nota = prompt("digite sua nota")
+const media = 6
+let presente = prompt("digite dias presentes")
+const presença = 330
+
+if(nota >= media || presente >= presença){
+document.write('APROVADO')
+}
+else{
+document.write('REPROVADO')
+}*/
+
+/* operador "negação" ! inverte o resultado da express~]ao de comparação
+
+let nota = prompt("digite sua nota")
+const media = 6
+let presente = prompt("digite dias presentes")
+const presença = 330
+
+if(!(nota >= media && presente >= presença)){
+document.write('APROVADO')
+}
+else{
+document.write('REPROVADO')
+}
+*/
+
+let nota = prompt('digite sua nota:')
+let faltas = prompt('digite a quantidade de faltas:')
+
+const media = 6
+const faltas_maximas = 15
+
+if(nota >= media && faltas <= faltas_maximas){
+document.write('APROVADO')
+}
+else{
+document.write('REPROVADO')
+}
+
+
+
