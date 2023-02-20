14fcb153b4746c29de982ec098a321b2ea52d85b
unknown <ukssbrs@gmail.com>
2023-02-19 22:25:55 -0300

    Operadores-Aritméticos-Atribuição-De-Valores...

--- aula12/index.js
+++ aula12/index.js
@@ -7,7 +7,7 @@ let idade = 1
 switch(idade){
     case 1:
         document.write('linha 1')
-        break /* o comando funciona uma queda d´agua e o break ele funciona como um freio para a queda ser parada no local onde coloco o break*/
+        break /* o comando funciona como uma queda d´agua e o break ele funciona como um freio para a queda ser parada no local onde coloco o break*/
 
     case 2: document.write('linha da 2')
         break
---
+++ aula13/operação.aritmeticas.atribuição.valores/index.js
@@ -0,0 +1,13 @@
+let test = 'Fala '
+
+//test = test + 5
+//test += 5
+//test -= 5
+//test *= 5
+//test /= 2
+//test %= 3
+
+//podemos atribuir valores nas strings. ex:
+test += 'Lucas'
+
+document.write(test)
