04cbbeb40dabd97bc22957704a6602c11b4faf39
unknown <ukssbrs@gmail.com>
2023-02-22 21:42:52 -0300

    função

---
+++ aula15/index.js
@@ -0,0 +1,24 @@
+/*let largura = Number(prompt('Digite a Largura'))
+let comprimento = Number(prompt('Digite o comprimento'))
+
+function CalculandoAreaTerreno(largura, comprimento){
+
+    var area = largura*comprimento
+    
+    return area
+}
+
+
+let area = CalculandoAreaTerreno(largura, comprimento)
+
+document.write('o terreno possui' + area + ' metros quadrados')*/
+
+ function soma(a, b){
+   //b = b === underfined ? 0 : b
+    return a + b
+ }
+
+ console.log(soma(7, 7))
+ console.log(soma(7, 7, 9, 15)) //desconsidera os parâmetros adicionais
+ console.log(soma(7)) 
+ console.log(soma(0))
