efa6be46d121f1905b5b8e07a8b8d958b3bc1ec0
unknown <ukssbrs@gmail.com>
2023-03-16 14:57:56 -0300

    ajustes

---
+++ 8 DOM/2 DOM-selecionando.elementos.pagina/script.js
@@ -0,0 +1,16 @@
+//document( API DOM )
+/*
+getElementById() = indentificador unido
+getElementsByTagName()
+getElementsByClassName()
+getElementsByName()
+ */
+
+
+//console.log(document.getElementById('nome_usuario'))
+
+//console.log(document.getElementsByTagName('input'))
+
+//console.log(document.getElementsByClassName('campo_texto'))
+
+console.log(document.getElementsByName('name'))
