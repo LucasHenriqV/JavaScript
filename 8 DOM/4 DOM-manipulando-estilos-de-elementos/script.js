31d41a7919bfedee73e260868d5b8069e16c0dd8
unknown <ukssbrs@gmail.com>
2023-03-17 19:47:17 -0300

    ajustes

---
+++ 8 DOM/4 DOM-manipulando-estilos-de-elementos/script.js
@@ -0,0 +1,8 @@
+function modificaDiv(corDeFundo, l, a) {
+    document.getElementById('div').style.background = corDeFundo
+
+    document.getElementById('div').style.width = l 
+
+    document.getElementById('div').style.height = a
+    
+}
